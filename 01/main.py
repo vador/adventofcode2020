7370573dda837a85589d5d9e677cbf64c806dbaa
Damien Salvador <damien.salvador@publicissapient.com>
2020-12-01 14:44:53 +0100

    cleanup/reformat

--- 01/main.py
+++ 01/main.py
@@ -5,18 +5,16 @@ expenses = lv.list_to_intlist()
 
 print(expenses)
 
-
 year = 2020
 
-while (len(expenses)>0):
+while len(expenses) > 0:
     first = expenses.pop()
     exp_set = set(expenses)
     rest = year - first
-    while len(exp_set)>0:
+    while len(exp_set) > 0:
         second = exp_set.pop()
 
         third = rest - second
 
-        if (third in exp_set):
+        if third in exp_set:
             print(first, second, third, second * third * first)
-
