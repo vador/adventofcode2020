c5285db0e45e5f45c876b5c43557bad5675b7966
Damien Salvador <damien.salvador@publicissapient.com>
2020-12-06 17:55:20 +0100

    Attempted unit tests

---
+++ 04/test_passport.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+
+class TestPassport(TestCase):
+    def test_isvalid_passport(self):
+        self.fail()
+
+    def test_are_compulsory_keys_present(self):
+        self.fail()
+
+    def test_are_passportkeys_valid(self):
+        self.fail()
+
+    def test_validate_hcl(self):
+        self.fail()
+
+    def test_validate_hgt(self):
+        self.fail()
+
+    def test_validate_pid(self):
+        self.fail()
+
+    def test_validate_ecl(self):
+        pass
+
+    def test_validate_eyr(self):
+        pass
+
+    def test_validate_iyr(self):
+        pass
+
+    def test_validate_byr(self):
+        pass
