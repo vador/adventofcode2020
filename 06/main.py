e1c5c7cd3d661fc245bd698a2e6ac9e73303a97c
Damien Salvador <damien.salvador@publicissapient.com>
2020-12-06 06:39:11 +0100

    Day 6 2 stars

---
+++ 06/main.py
@@ -0,0 +1,45 @@
+import logging
+
+from loadValues import LoadValues
+
+
+def list_questions_for_group_union(group):
+    result = set()
+    for person in group:
+        res_person = set(person)
+        logging.debug(res_person)
+        result = result.union(res_person)
+    logging.debug("Result group:" + str(result))
+    return len(result)
+
+
+def list_questions_for_group_intersection(group):
+    result = set(list("abcdefghijklmnopqrstuvwxyz"))
+    for person in group:
+        res_person = set(person)
+        logging.debug(res_person)
+        result = result.intersection(res_person)
+    logging.debug("Result group:" + str(result))
+    return len(result)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Started')
+    lv = LoadValues()
+    lv.strip_lines()
+    groups = lv.get_groups()
+    logging.debug(groups)
+    res = [(list_questions_for_group_union(group)) for group in groups]
+    cnt = sum(res)
+    print("Star 1 : ", cnt)
+    res = [(list_questions_for_group_intersection(group)) for group in groups]
+    cnt = sum(res)
+    print("Star 2 : ", cnt)
+
+    logging.info('Finished')
+
+
+##
+if __name__ == '__main__':
+    main()
