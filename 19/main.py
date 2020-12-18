8d19d987a4c8f72b2ccbece2d2e0e9ae51fe416e
Damien Salvador <damien.salvador@publicissapient.com>
2020-12-18 13:48:08 +0100

    Day 19 init

---
+++ 19/main.py
@@ -0,0 +1,32 @@
+import logging
+import re
+import cProfile
+
+from loadValues import LoadValues
+
+
+def main():
+    number = 0
+
+    lv = LoadValues()
+
+    number = 0
+
+    print("Star 1 : ", number)
+
+    print("Star 2 : ", number)
+    logging.info('Finished')
+
+
+##
+if __name__ == '__main__':
+    pr = cProfile.Profile()
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('Started')
+    pr.enable()
+
+    main()
+    pr.disable()
+
+    logging.info('Finished')
+    # pr.print_stats()
