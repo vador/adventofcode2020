9892421f093eecd4d50bc0d4b18da97d8eba9b2f
Damien Salvador <damien.salvador@publicissapient.com>
2020-12-20 11:37:46 +0100

    refacto with classes

--- 20/main.py
+++ 20/main.py
@@ -2,69 +2,81 @@ import logging
 import re
 import cProfile
 import math
-
+from collections import namedtuple
 from loadValues import LoadValues
 
+DIRECTIONS = ['U', 'R', 'D', 'L']
 
-def parse_tile(tile):
-    tile_id = tile[0][5:-1]
-    up = tile[1]
-    down = tile[-1]
-    left = []
-    right = []
-    for line in tile[1:]:
-        left.append(line[0])
-        right.append(line[-1])
-    return tile_id, up, ''.join(right), down[::-1], ''.join(left)[::-1]
+Side = namedtuple('Side', ['tile', 'direction', 'flipped', 'side'])
 
 
-dir = dict()
-all_sides = dict()
-tiles = dict()
+class Tile:
+    tildeId = None
+    tile = None
+    direction = None
+    flipped = None
+
+    def __init__(self, tile):
+        tile_id = tile[0][5:-1]
+        self.tildeId = int(tile_id)
+        self.tile = tile[1:].copy()
+
+    def get_all_sides(self):
+        tile = self.tile
+        up = tile[0]
+        down = tile[-1][::-1]
+        left = []
+        right = []
+        for line in tile:
+            left.append(line[0])
+            right.append(line[-1])
+        right = ''.join(right)
+        left = ''.join(left)[::-1]
+        sides = [Side(self.tildeId, i, False, side) for (i, side) in enumerate([up, left, down, right])]
+        sidesRev = [Side(self.tildeId, i, True, side[::-1]) for (i, side) in enumerate([up, right, down, left])]
+        return sides + sidesRev
 
 
-def add_tile_to_list(tile):
-    tile_id, up, right, down, left = tile
-    for side in (up, right, down, left):
-        if side in all_sides:
-            all_sides[side].append(tile_id)
-        else:
-            all_sides[side] = [tile_id]
-        revside = side[::-1]
-        if revside in all_sides:
-            all_sides[revside].append(tile_id)
-        else:
-            all_sides[revside] = [tile_id]
+class TileList:
+    tiles = None
+    all_sides = None
+
+    def __init__(self):
+        self.tiles = {}
+        self.all_sides = {}
+
+    def add_tile(self, tile):
+        self.tiles[tile.tildeId] = tile
+        for side in tile.get_all_sides():
+            if side.side in self.all_sides:
+                self.all_sides[side.side].append(side)
+            else:
+                self.all_sides[side.side] = [side]
 
 
 def main():
     number = 0
 
-    lv = LoadValues("input.txt", groups=True)
+    lv = LoadValues("test.txt", groups=True)
 
     lst = set()
+    TL = TileList()
     for tile in lv.raw_values:
-        t = parse_tile(tile)
-        logging.debug(t)
-        add_tile_to_list(t)
-        tile_id, up, right, down, left = t
+        T = Tile(tile)
+        TL.add_tile(T)
 
-        lst |= {up, down, right, left}
-
-    print(len(lv.raw_values))
-    print(len(lst))
-    print(all_sides)
-    res = {}
-    for side in all_sides:
-        tiles = all_sides[side]
-        if len(tiles) == 1:
-            if tiles[0] in res:
-                res[tiles[0]] += 1
-            else:
-                res[tiles[0]] = 1
-    print([int(tile) for tile in res if res[tile] == 4])
-
-    number = math.prod([int(tile) for tile in res if res[tile] == 4])
+    logging.debug(TL.all_sides)
+    tiles_uniqueside = [[s.tile for s in sides][0] for (strside, sides) in TL.all_sides.items() if len(sides) == 1]
+    logging.debug(tiles_uniqueside)
+    tiles_countunique = {}
+    for tile in tiles_uniqueside:
+        if tile in tiles_countunique:
+            tiles_countunique[tile] += 1
+        else:
+            tiles_countunique[tile] = 1
+    corners = set([tile for (tile, cnt) in tiles_countunique.items() if cnt == 4])
+    logging.debug(corners)
+    number = math.prod(corners)
     print("Star 1 : ", number)
 
     print("Star 2 : ", number)
